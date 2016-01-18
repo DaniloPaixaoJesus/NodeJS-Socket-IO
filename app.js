bdd4596fac60014f82b786404ad9e3f59d1e10ae
danilopaixao <danilo.oficial@gmail.com>
2016-01-18 11:03:39 -0200

    commit session control

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ var express = require('express')
 , load = require('express-load')
 , app = express();
 
+
+//stack of configuration
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+//use control session and cookies
+app.use(express.cookieParser('ntalk'));
+app.use(express.session());
+
+//responsable for create objects JSON from form HTML
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use(express.static(__dirname + '/public'));
-//app.get('/', routes.index);
-//app.get('/usuarios', routes.user.index);
+
 
 load('models')
 .then('controllers')
