bd830f4135bce765c8c03ae983764fab99e71277
danilopaixao <danilo.oficial@gmail.com>
2016-01-19 21:42:49 -0200

    correct bug socket io url server in the client

---
+++ public/javascripts/util.js
@@ -0,0 +1,11 @@
+function getUrl(){	
+	var url = window.location.href;	
+	var arr = url.split("//");
+	var port = '';	
+	if(location.port != ''){
+		port = ':' + location.port;
+	}
+	var str_url = arr[0] + '//' + location.hostname + port;
+	//alert(str_url);
+	return str_url;
+}
