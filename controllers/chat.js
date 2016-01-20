6daf1bcbee940f9a570dc2c15fe004a2bf1ab8e3
Danilo Paixão de Jesus <danilo.oficial@gmail.com>
2016-01-20 17:57:14 -0200

    Update chat.js
    refactory

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,9 +1,12 @@
 module.exports = function(app) {
 	var ChatController = {
 		index: function(req, res){
-			var resultado = {email: req.params.email,
-			usuario: req.session.usuario};
-			res.render('chat/index', resultado);
+			/*var resultado = {email: req.params.email,
+					usuario: req.session.usuario
+				
+			};*/
+			var param = {email:req.params.email};
+			res.render('chat/index', param /*resultado*/);
 		}
 	};
 	return ChatController;
